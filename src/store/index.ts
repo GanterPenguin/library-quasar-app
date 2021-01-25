43ae247f460679b2a99c538c5c1ffe76da12c217
Ivan Buldakov <iv081198@yandex.ru>
2021-01-25 08:17:44 +0300

    update store init

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,11 @@
-import { store } from 'quasar/wrappers';
 import Vuex from 'vuex';
+import Vue from 'vue';
+import { config } from 'vuex-module-decorators';
 
-// import example from './module-example';
-// import { ExampleStateInterface } from './module-example/state';
+config.rawError = true;
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation
- */
+Vue.use(Vuex);
 
-export interface StateInterface {
-  // Define your own store structure, using submodules if needed
-  // example: ExampleStateInterface;
-  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  example: unknown;
-}
-
-export default store(({ Vue }) => {
-  Vue.use(Vuex);
-
-  const Store = new Vuex.Store<StateInterface>({
-    modules: {
-      // example
-    },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    strict: !!process.env.DEBUGGING,
-  });
-
-  return Store;
+export default new Vuex.Store({
+  strict: !!process.env.DEV,
 });
