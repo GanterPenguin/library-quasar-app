f6728aa69d6a97ccc22b5ab5a7304d31650faa46
Ivan Buldakov <iv081198@yandex.ru>
2021-01-25 08:13:01 +0300

    add library type

---
+++ src/models/Library.ts
@@ -0,0 +1,50 @@
+export interface Image {
+  url: string;
+  title: string;
+}
+
+export interface Address {
+  street: string;
+  comment: string;
+  fullAddress: string;
+}
+
+export interface Phone {
+  value: number;
+  comment: string;
+}
+
+export interface Tag {
+  id: number;
+  name: string;
+  sysName: string;
+}
+
+export interface Library {
+  _id: string;
+  nativeId: number;
+  data: {
+    info: unknown;
+    general: {
+      id: number;
+      name: string;
+      description: string;
+      status: string;
+      address: Address;
+      contacts: {
+        website: 'string';
+        email: string;
+        phones: Phone[];
+      };
+      gallery?: Image[];
+      image?: Image;
+    };
+    tags: Tag[];
+    workingSchedule: {
+      [key: string]: {
+        from: string;
+        to: string;
+      }
+    };
+  };
+}
