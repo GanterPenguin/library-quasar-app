fbb124b214f17ff76f9ca5fa7ad29a203a5fb6b7
Ivan Buldakov <iv081198@yandex.ru>
2021-01-25 08:31:26 +0300

    add detail layout

---
+++ src/layouts/DetailLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="arrow_back"
+          aria-label="back"
+          @click="$router.back()"
+        />
+        <q-toolbar-title>
+          Деталка библиотеки
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'DetailLayout',
+});
+</script>
