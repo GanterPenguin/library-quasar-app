ae44c24c9bb03bfa8b95817d67cc30c607953c36
Ivan Buldakov <iv081198@yandex.ru>
2021-01-25 08:21:46 +0300

    add pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="q-pa-lg flex flex-center">
+    <q-pagination
+      v-if="page && maxPages"
+      v-model="page"
+      :max="maxPages"
+      :max-pages="5"
+      :direction-links="true"
+    >
+    </q-pagination>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from '@vue/composition-api';
+import { libraryListModule } from 'store/modules/libraryList';
+import { queryParamsModule } from 'store/modules/queryParams';
+
+export default defineComponent({
+  name: 'Pagination',
+  setup() {
+    const page = computed({
+      get() {
+        if (queryParamsModule.params._page) {
+          return queryParamsModule.params._page as number;
+        }
+        return 1;
+      },
+      set(value: number) {
+        queryParamsModule.updateParams({ _page: value });
+      },
+    });
+
+    const maxPages = computed(() => libraryListModule.totalPages);
+
+    return { page, maxPages };
+  },
+});
+</script>
