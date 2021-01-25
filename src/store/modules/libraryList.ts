8581e42242e15cb29b150ce8b7aa3c523cf32aca
Ivan Buldakov <iv081198@yandex.ru>
2021-01-25 08:10:26 +0300

    add libraryList store module

---
+++ src/store/modules/libraryList.ts
@@ -0,0 +1,47 @@
+import {
+  VuexModule, Module, Action, Mutation, getModule,
+} from 'vuex-module-decorators';
+import store from 'store/index';
+import { Library } from 'models/Library';
+import { getLibrariesList } from 'api/requests';
+import { SearchParams } from 'models/filters';
+import { getTotalPagesFromHeaders } from 'lib/utils';
+
+interface LibraryListInterface {
+  items: Library[];
+  totalPages: number | null;
+}
+
+@Module({
+  dynamic: true,
+  store,
+  name: 'library-list',
+  namespaced: true,
+})
+class LibraryList extends VuexModule implements LibraryListInterface {
+  public items: Library[] = [];
+
+  public totalPages: number | null = null;
+
+  @Action
+  public async getItems(params: SearchParams) {
+    const response = await getLibrariesList(params);
+    this.SET_ITEMS(response.data);
+    const totalPages = getTotalPagesFromHeaders(response.headers);
+    if (totalPages) {
+      this.SET_TOTAL_PAGES(totalPages);
+    }
+  }
+
+  @Mutation
+  private SET_ITEMS(items: Library[]) {
+    this.items = items;
+  }
+
+  @Mutation
+  private SET_TOTAL_PAGES(pages: number) {
+    this.totalPages = pages;
+  }
+}
+
+export const libraryListModule = getModule(LibraryList);
