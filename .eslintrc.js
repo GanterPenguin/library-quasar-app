bb056a9ebab3fa980165fdfc5a3dd66b4efde22f
Ivan Buldakov <iv081198@yandex.ru>
2021-01-25 08:06:35 +0300

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -79,6 +79,8 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
     'prefer-promise-reject-errors': 'off',
+    'no-underscore-dangle': 'off',
+    'no-void': 'off',
 
     // TypeScript
     quotes: ['warn', 'single', { avoidEscape: true }],
