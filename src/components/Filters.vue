41be76aa988b4438ed4df833f55bc0d9c07ef0fe
Ivan Buldakov <iv081198@yandex.ru>
2021-01-26 07:25:32 +0300

    update filters component

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,14 +1,63 @@
 <template>
   <div class="filters q-pa-md">
-    Filters
+    <q-input
+      v-model="filters.q"
+      label="Поиск"
+    />
+    <q-select
+      v-model="filters._sort"
+      label="Сортировка по"
+      :options="sortOptions"
+      emit-value
+      map-options
+    />
   </div>
 </template>
 
-<script>
-import { defineComponent } from '@vue/composition-api';
+<script lang="ts">
+import { defineComponent, ref, watch } from '@vue/composition-api';
+import { debounce } from 'quasar';
+import { queryParamsModule } from 'store/modules/queryParams';
 
 export default defineComponent({
   name: 'Filters',
+  setup() {
+    const filters = ref({
+      q: null,
+      _sort: null,
+    });
+
+    const applyFilters = debounce(() => {
+      queryParamsModule.updateParams({ ...filters.value });
+    }, 300);
+
+    watch(filters, applyFilters, { deep: true });
+
+    const sortOptions = [
+      {
+        label: 'Не задана',
+        value: null,
+      },
+      {
+        label: 'Дата создания',
+        value: 'data.info.createDate',
+      },
+      {
+        label: 'Дата обновления',
+        value: 'data.info.updateDate',
+      },
+      {
+        label: 'Название',
+        value: 'data.general.name',
+      },
+      {
+        label: 'ID',
+        value: 'data.general.id',
+      },
+    ];
+
+    return { filters, applyFilters, sortOptions };
+  },
 });
 </script>
 
